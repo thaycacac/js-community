8dbf9f93f57a3941069d3a232807055e697d2f1d
wasd <nguyenquanglinh98@gmail.com>
2018-08-23 09:02:38 +0700

    Done writing post

---
+++ src/pages/WritePost/Editor2.js
@@ -0,0 +1,538 @@
+import { Editor, getEventRange, getEventTransfer  } from 'slate-react'
+import { Block,Value } from 'slate'
+import styled from 'react-emotion'
+import { LAST_CHILD_TYPE_INVALID } from 'slate-schema-violations'
+import React from 'react'
+import initialValue from './value.json'
+import imageExtensions from 'image-extensions'
+import isUrl from 'is-url'
+
+import { isKeyHotkey } from 'is-hotkey'
+import { Button, Icon, Toolbar } from '../components'
+
+/**
+ * A change helper to standardize wrapping links.
+ *
+ * @param {Change} change
+ * @param {String} href
+ */
+
+function wrapLink(change, href) {
+    change.wrapInline({
+      type: 'link',
+      data: { href },
+    })
+  
+    change.moveToEnd()
+  }
+  
+  /**
+   * A change helper to standardize unwrapping links.
+   *
+   * @param {Change} change
+   */
+  
+  function unwrapLink(change) {
+    change.unwrapInline('link')
+  }
+  
+/**
+ * Define the default node type.
+ *
+ * @type {String}
+ */
+
+const DEFAULT_NODE = 'paragraph'
+
+/**
+ * Define hotkey matchers.
+ *
+ * @type {Function}
+ */
+
+const isBoldHotkey = isKeyHotkey('mod+b')
+const isItalicHotkey = isKeyHotkey('mod+i')
+const isUnderlinedHotkey = isKeyHotkey('mod+u')
+const isCodeHotkey = isKeyHotkey('mod+`')
+
+/**
+ * The rich text example.
+ *
+ * @type {Component}
+ */
+
+ /**
+ * A styled image block component.
+ *
+ * @type {Component}
+ */
+
+const Image = styled('img')`
+display: block;
+max-width: 100%;
+max-height: 20em;
+box-shadow: ${props => (props.selected ? '0 0 0 2px blue;' : 'none')};
+`
+
+/*
+* A function to determine whether a URL has an image extension.
+*
+* @param {String} url
+* @return {Boolean}
+*/
+
+function isImage(url) {
+return !!imageExtensions.find(url.endsWith)
+}
+
+/**
+* A change function to standardize inserting images.
+*
+* @param {Change} change
+* @param {String} src
+* @param {Range} target
+*/
+
+function insertImage(change, src, target) {
+if (target) {
+  change.select(target)
+}
+
+change.insertBlock({
+  type: 'image',
+  isVoid: true,
+  data: { src },
+})
+}
+
+/**
+* A schema to enforce that there's always a paragraph as the last block.
+*
+* @type {Object}
+*/
+
+const schema = {
+document: {
+  last: { type: 'paragraph' },
+  normalize: (change, { code, node, child }) => {
+    switch (code) {
+      case LAST_CHILD_TYPE_INVALID: {
+        const paragraph = Block.create('paragraph')
+        return change.insertNodeByKey(node.key, node.nodes.size, paragraph)
+      }
+    }
+  },
+},
+}
+
+
+class RichText extends React.Component {
+  /**
+   * Deserialize the initial editor value.
+   *
+   * @type {Object}
+   */
+
+  state = {
+    value: Value.fromJSON(initialValue),
+  }
+
+  /**
+   * Check whether the current selection has a link in it.
+   *
+   * @return {Boolean} hasLinks
+   */
+
+  hasLinks = () => {
+    const { value } = this.state
+    return value.inlines.some(inline => inline.type == 'link')
+  }
+
+  /**
+   * Check if the current selection has a mark with `type` in it.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  hasMark = type => {
+    const { value } = this.state
+    return value.activeMarks.some(mark => mark.type == type)
+  }
+
+  /**
+   * Check if the any of the currently selected blocks are of `type`.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  hasBlock = type => {
+    const { value } = this.state
+    return value.blocks.some(node => node.type == type)
+  }
+
+  /**
+   * Render.
+   *
+   * @return {Element}
+   */
+
+  render() {
+    return (
+      <div>
+        <Toolbar>
+          {this.renderMarkButton('bold', <b>B</b>)}
+          {this.renderMarkButton('italic', <i>I</i>)}
+          {this.renderMarkButton('underlined', <u>U</u>)}
+          {this.renderMarkButton('code', '<>')}
+          {this.renderBlockButton('heading-one', 'h1')}
+          {this.renderBlockButton('heading-two', 'h2')}
+          {this.renderBlockButton('block-quote', "quote")}
+          {this.renderBlockButton('numbered-list', 'ul')}
+          {this.renderBlockButton('bulleted-list', 'ol')}
+          <Button onMouseDown={this.onClickImage}>
+            <Icon><i class="fa fa-file-image-o"></i></Icon>
+          </Button>
+          <Button active={this.hasLinks()} onMouseDown={this.onClickLink}>
+            <Icon><i class="fa fa-link"></i></Icon>
+          </Button>
+        </Toolbar>
+        <Editor
+          spellCheck
+          autoFocus
+          placeholder="Enter content here..."
+          value={this.state.value}
+          schema={schema}
+          onChange={this.onChange}
+          onDrop={this.onDropOrPaste}
+          onPaste={this.onDropOrPaste}
+          onKeyDown={this.onKeyDown}
+          renderNode={this.renderNode}
+          renderMark={this.renderMark}
+          
+        />
+      </div>
+    )
+  }
+
+  /**
+   * Render a mark-toggling toolbar button.
+   *
+   * @param {String} type
+   * @param {String} icon
+   * @return {Element}
+   */
+
+  renderMarkButton = (type, icon) => {
+    const isActive = this.hasMark(type)
+
+    return (
+      <Button
+        active={isActive}
+        onMouseDown={event => this.onClickMark(event, type)}
+      >
+        <Icon>{icon}</Icon>
+      </Button>
+    )
+  }
+
+  /**
+   * Render a block-toggling toolbar button.
+   *
+   * @param {String} type
+   * @param {String} icon
+   * @return {Element}
+   */
+
+  renderBlockButton = (type, icon) => {
+    let isActive = this.hasBlock(type)
+
+    if (['numbered-list', 'bulleted-list'].includes(type)) {
+      const { value } = this.state
+      const parent = value.document.getParent(value.blocks.first().key)
+      isActive = this.hasBlock('list-item') && parent && parent.type === type
+    }
+
+    return (
+      <Button
+        active={isActive}
+        onMouseDown={event => this.onClickBlock(event, type)}
+      >
+        <Icon>{icon}</Icon>
+      </Button>
+    )
+  }
+
+  /**
+   * Render a Slate node.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  renderNode = props => {
+    const { attributes, children, node , isFocused} = props
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>
+      case 'list-item':
+        return <li {...attributes}>{children}</li>
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>
+        case 'image': {
+            const src = node.data.get('src')
+            return <Image src={src} selected={isFocused} {...attributes} />
+          }
+          case 'link': {
+            const { data } = node
+            const href = data.get('href')
+            return (
+              <a {...attributes} href={href}>
+                {children}
+              </a>
+            )
+          }
+    }
+  }
+
+  onClickImage = event => {
+    event.preventDefault()
+    const src = window.prompt('Enter the URL of the image:')
+    if (!src) return
+
+    const change = this.state.value.change().call(insertImage, src)
+
+    this.onChange(change)
+  }
+
+  /**
+   * On drop, insert the image wherever it is dropped.
+   *
+   * @param {Event} event
+   * @param {Change} change
+   * @param {Editor} editor
+   */
+
+  onDropOrPaste = (event, change, editor) => {
+    const target = getEventRange(event, change.value)
+    if (!target && event.type == 'drop') return
+
+    const transfer = getEventTransfer(event)
+    const { type, text, files } = transfer
+
+    if (type == 'files') {
+      for (const file of files) {
+        const reader = new FileReader()
+        const [mime] = file.type.split('/')
+        if (mime != 'image') continue
+
+        reader.addEventListener('load', () => {
+          editor.change(c => {
+            c.call(insertImage, reader.result, target)
+          })
+        })
+
+        reader.readAsDataURL(file)
+      }
+    }
+
+    if (type == 'text') {
+      if (!isUrl(text)) return
+      if (!isImage(text)) return
+      change.call(insertImage, text, target)
+    }
+  }
+  
+  /**
+   * Render a Slate mark.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  renderMark = props => {
+    const { children, mark, attributes } = props
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>
+      case 'code':
+        return <code {...attributes}>{children}</code>
+      case 'italic':
+        return <em {...attributes}>{children}</em>
+      case 'underlined':
+        return <u {...attributes}>{children}</u>
+    }
+  }
+
+  /**
+   * On change, save the new `value`.
+   *
+   * @param {Change} change
+   */
+
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
+  /**
+   * On key down, if it's a formatting command toggle a mark.
+   *
+   * @param {Event} event
+   * @param {Change} change
+   * @return {Change}
+   */
+
+  onKeyDown = (event, change) => {
+    let mark
+
+    if (isBoldHotkey(event)) {
+      mark = 'bold'
+    } else if (isItalicHotkey(event)) {
+      mark = 'italic'
+    } else if (isUnderlinedHotkey(event)) {
+      mark = 'underlined'
+    } else if (isCodeHotkey(event)) {
+      mark = 'code'
+    } else {
+      return
+    }
+
+    event.preventDefault()
+    change.toggleMark(mark)
+    return true
+  }
+
+  /**
+   * When a mark button is clicked, toggle the current mark.
+   *
+   * @param {Event} event
+   * @param {String} type
+   */
+
+  onClickMark = (event, type) => {
+    event.preventDefault()
+    const { value } = this.state
+    const change = value.change().toggleMark(type)
+    this.onChange(change)
+  }
+
+  /**
+   * When a block button is clicked, toggle the block type.
+   *
+   * @param {Event} event
+   * @param {String} type
+   */
+
+  onClickBlock = (event, type) => {
+    event.preventDefault()
+    const { value } = this.state
+    const change = value.change()
+    const { document } = value
+
+    // Handle everything but list buttons.
+    if (type != 'bulleted-list' && type != 'numbered-list') {
+      const isActive = this.hasBlock(type)
+      const isList = this.hasBlock('list-item')
+
+      if (isList) {
+        change
+          .setBlocks(isActive ? DEFAULT_NODE : type)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list')
+      } else {
+        change.setBlocks(isActive ? DEFAULT_NODE : type)
+      }
+    } else {
+      // Handle the extra wrapping required for list buttons.
+      const isList = this.hasBlock('list-item')
+      const isType = value.blocks.some(block => {
+        return !!document.getClosest(block.key, parent => parent.type == type)
+      })
+
+      if (isList && isType) {
+        change
+          .setBlocks(DEFAULT_NODE)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list')
+      } else if (isList) {
+        change
+          .unwrapBlock(
+            type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+          )
+          .wrapBlock(type)
+      } else {
+        change.setBlocks('list-item').wrapBlock(type)
+      }
+    }
+
+    this.onChange(change)
+  }
+  /**
+   * When clicking a link, if the selection has a link in it, remove the link.
+   * Otherwise, add a new link with an href and text.
+   *
+   * @param {Event} event
+   */
+
+  onClickLink = event => {
+    event.preventDefault()
+    const { value } = this.state
+    const hasLinks = this.hasLinks()
+    const change = value.change()
+
+    if (hasLinks) {
+      change.call(unwrapLink)
+    } else if (value.isExpanded) {
+      const href = window.prompt('Enter the URL of the link:')
+      change.call(wrapLink, href)
+    } else {
+      const href = window.prompt('Enter the URL of the link:')
+      const text = window.prompt('Enter the text for the link:')
+
+      change
+        .insertText(text)
+        .extend(0 - text.length)
+        .call(wrapLink, href)
+    }
+
+    this.onChange(change)
+  }
+
+  /**
+   * On paste, if the text is a link, wrap the selection in a link.
+   *
+   * @param {Event} event
+   * @param {Change} change
+   */
+
+  onPaste = (event, change) => {
+    if (change.value.selection.isCollapsed) return
+
+    const transfer = getEventTransfer(event)
+    const { type, text } = transfer
+    if (type != 'text' && type != 'html') return
+    if (!isUrl(text)) return
+
+    if (this.hasLinks()) {
+      change.call(unwrapLink)
+    }
+
+    change.call(wrapLink, text)
+    return true
+  }
+}
+
+/**
+ * Export.
+ */
+
+export default RichText
--- src/pages/WritePost/WritePost.js
+++ src/pages/WritePost/WritePost.js
@@ -1,84 +1,18 @@
 import React, { Component } from 'react';
 // Require Editor JS files.
-import { Editor } from 'slate-react';
-import { Value } from 'slate';
+
 import './WritePost.css';
 import Header from './../header/Header';
 import Menu from './../Menu/Menu';
+import Editor1 from './Editor1';
+import Editor2 from './Editor2';
 
-const existingValue = JSON.parse(localStorage.getItem('content'))
-const initialValue = Value.fromJSON(
-  existingValue || {
-    document: {
-      nodes: [
-        {
-          object: 'block',
-          type: 'paragraph',
-          nodes: [
-            {
-              object: 'text',
-              leaves: [
-                {
-                  text: '',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  }
-)
-
-function CodeNode(props) {
-    return (
-      <pre {...props.attributes}>
-        <code>{props.children}</code>
-      </pre>
-    )
-  }
-
-  const boldPlugin = MarkHotkey({
-    type: 'bold',
-    key: 'b',
-  })
-// Initialize a plugin for each mark...
-  const plugins = [
-    MarkHotkey({ key: 'b', type: 'bold' }),
-    MarkHotkey({ key: '`', type: 'code' }),
-    MarkHotkey({ key: 'i', type: 'italic' }),
-    MarkHotkey({ key: '~', type: 'strikethrough' }),
-    MarkHotkey({ key: 'u', type: 'underline' }),
-]
-
-  function MarkHotkey(options) {
-    const { type, key } = options
-
-    return {
-      onKeyDown(event, change) {
-        if (!event.ctrlKey || event.key != key) return
-        event.preventDefault()
-        change.toggleMark(type)
-        return true
-      },
-    }
-  }
 export default class WritePost extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            value: initialValue,
-        }
     }
-
-    onChange = ({ value }) => {
-        // Check to see if the document has changed before saving.
-        if (value.document != this.state.value.document) {
-          const content = JSON.stringify(value.toJSON())
-          localStorage.setItem('content', content)
-        }
-        this.setState({ value })
-      }
+    
+    
   
     render() {
         return (
@@ -104,33 +38,17 @@ export default class WritePost extends Component {
                             <input className='title-input' type='text' placeholder='Title'/>
                         </form>
                     </div>
-
-                    
                     <div className='writing-content'>
-                    <Editor 
-                    plugins={plugins}
-                    value={this.state.value}
-                    onChange={this.onChange}
-                    renderMark={this.renderMark}
-                    placeholder='Enter content here...'
-                    />
+                    <Editor2/>
                     </div>
-                    <button 
-                    className='btn btn-success btn-clear'
-                    onClick={()=>{
-                        localStorage.clear();
-                        alert('Clear successfully!');
-                        window.location.reload();
-                    }}
-                    >
-                    Clear
-                    </button>
+                    
                     <div className='writing-hint'>
                         <code>ctrl B</code> - <strong>bold</strong><br/>
                         <code>ctrl I</code> - <i>italy</i><br/>
                         <code>ctrl U</code> - <u>underline</u><br/>
                         <code>ctrl `</code> - <code>code</code><br/>
-                        <i>The content is saved as draft automatically.</i><br/>
+                        <i>You must select the text before editing link</i><br/>
+
                     </div>
                     </div>
                 </div>
@@ -138,26 +56,5 @@ export default class WritePost extends Component {
         )
     }
 
-    renderNode = props => {
-        switch (props.node.type) {
-          case 'code':
-            return <CodeNode {...props} />
-        }
-      }
     
-      renderMark = props => {
-        switch (props.mark.type) {
-          case 'bold':
-            return <strong>{props.children}</strong>
-          // Add our new mark renderers...
-          case 'code':
-            return <code>{props.children}</code>
-          case 'italic':
-            return <em>{props.children}</em>
-          case 'strikethrough':
-            return <del>{props.children}</del>
-          case 'underline':
-            return <u>{props.children}</u>
-        }
-      }
 };
