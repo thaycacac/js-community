f5e807f9417ea4ef61e63ec3c722a7f66bf8650b
wasd <nguyenquanglinh98@gmail.com>
2018-09-08 18:02:44 +0700

    done get all hashtags

--- src/reducers/hashtag/index.js
+++ src/reducers/hashtag/index.js
@@ -1,7 +1,9 @@
 
 import postHashtags from './reducerPostHashtags'
+import allHashtags from './reducerAllHashtags'
 import {combineReducers} from 'redux';
 
 export default combineReducers({
-    postHashtags
+    postHashtags,
+    allHashtags
 })
